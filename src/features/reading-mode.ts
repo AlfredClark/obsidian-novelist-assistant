b5d699b6c05f67644f3409c46099df13b41287a1
AlfredClark <alfredclark@163.com>
2026-05-29 15:27:45 +0800

    refactor: Refactor reading mode logic

--- src/features/reading-mode.ts
+++ src/features/reading-mode.ts
@@ -1,11 +1,33 @@
 import { ObsidianPlugin } from "../core/types";
 import { MarkdownPostProcessorContext } from "obsidian";
 
+function applyReadingModeStyles(plugin: ObsidianPlugin) {
+  if (plugin.settings.readingModeEnabled) {
+    window.document.documentElement.addClass("novel-reading-mode");
+    window.document.documentElement.style.setProperty(
+      "--novel-reading-mode-indent",
+      `${plugin.settings.readingModeIndent}rem`,
+    );
+    window.document.documentElement.style.setProperty(
+      "--novel-reading-mode-line-height",
+      `${plugin.settings.readingModeLineHeight}rem`,
+    );
+    window.document.documentElement.style.setProperty(
+      "--novel-reading-mode-paragraph-spacing",
+      `${plugin.settings.readingModeParagraphSpacing}rem`,
+    );
+  } else {
+    window.document.documentElement.removeClass("novel-reading-mode");
+  }
+}
+
 /**
  * Initialize the reading mode features
  * @param plugin ObsidianPlugin
  */
 export async function initializeReadingMode(plugin: ObsidianPlugin) {
+  applyReadingModeStyles(plugin);
+
   plugin.registerMarkdownPostProcessor((el: HTMLElement, ctx: MarkdownPostProcessorContext) => {
     // Skip setting library file
     if (
@@ -13,40 +35,23 @@ export async function initializeReadingMode(plugin: ObsidianPlugin) {
       ctx.sourcePath.startsWith(plugin.settings.settingLibraryFolder)
     )
       return;
-    if (plugin.settings.readingModeEnabled) {
-      let ps = el.getElementsByTagName("p");
-      let paragraphs: string[] = [];
-      const parser = new DOMParser();
-      for (let i = 0; i < ps.length; i++) {
-        ps.item(i)
-          ?.innerHTML.split("<br>")
-          .forEach((item) => {
-            paragraphs.push(
-              parser.parseFromString(item.trim(), "text/html").body.textContent.trim(),
-            );
-          });
-      }
-      el.empty();
-      paragraphs.forEach((paragraph) => {
-        el.createEl("p", {
-          text: paragraph,
+    if (!plugin.settings.readingModeEnabled) return;
+
+    const ps = el.getElementsByTagName("p");
+    const paragraphs: string[] = [];
+    const parser = new DOMParser();
+    for (let i = 0; i < ps.length; i++) {
+      ps.item(i)
+        ?.innerHTML.split("<br>")
+        .forEach((item) => {
+          paragraphs.push(parser.parseFromString(item.trim(), "text/html").body.textContent.trim());
         });
-      });
-      window.document.documentElement.addClass("novel-reading-mode");
-      window.document.documentElement.style.setProperty(
-        "--novel-reading-mode-indent",
-        `${plugin.settings.readingModeIndent}rem`,
-      );
-      window.document.documentElement.style.setProperty(
-        "--novel-reading-mode-line-height",
-        `${plugin.settings.readingModeLineHeight}rem`,
-      );
-      window.document.documentElement.style.setProperty(
-        "--novel-reading-mode-paragraph-spacing",
-        `${plugin.settings.readingModeParagraphSpacing}rem`,
-      );
-    } else {
-      window.document.documentElement.removeClass("novel-reading-mode");
     }
+    el.empty();
+    paragraphs.forEach((paragraph) => {
+      el.createEl("p", {
+        text: paragraph,
+      });
+    });
   });
 }
